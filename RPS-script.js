7a37326a1151eea316af5c91fd614eb0b3414b66
Yogii <stevenp0294@gmail.com>
2022-03-09 15:17:59 -0500

    function game() now fully plays game. Still need to be able to recall user and comp moves.

--- RPS-script.js
+++ RPS-script.js
@@ -1,4 +1,6 @@
 const move = ['rock', 'paper', 'scissors'];
+let score = 0;
+let compScore = 0;
 
 function computerPlay(){
     return move[Math.floor(Math.random() * move.length)];
@@ -10,16 +12,22 @@ function playRound(userSelection, compSelection){
     } else if (userSelection == null || ''){
         return "Come on. At least try!";
     } else if (userSelection == 'rock' && compSelection == 'paper'){
+        compScore = compScore + 1;
         return 'You lose! Paper covers Rock!';
     } else if (userSelection == 'rock' && compSelection == 'scissors'){
+        score = score + 1;
         return 'You win! Rock beats Scissors!';
     } else if (userSelection == 'paper' && compSelection == 'rock'){
+        score = score + 1;
         return 'You win! Paper covers Rock!';
     } else if (userSelection == 'paper' && compSelection == 'scissors'){
+        compScore = compScore + 1;
         return 'You lose! Scissors cut Paper!';
     } else if (userSelection == 'scissors' && compSelection == 'rock'){
+        compScore = compScore + 1;
         return 'You lose! Rock beats Scissors!';
     } else if (userSelection == 'scissors' && compSelection == 'paper'){
+        score = score + 1;
         return 'You win! Scissors cut Paper!';
     } else {
         return "Something has gone terribly wrong!";
@@ -30,10 +38,14 @@ function game(){
     for(let i = 0; i < 5; i++){
     let userSelection = prompt('Rock, Paper, or Scissors?', 'rock').toLowerCase();
     let compSelection = computerPlay();
-    console.log(userSelection);
-    console.log(compSelection);
-    console.log(playRound(userSelection, compSelection));
+    playRound(userSelection, compSelection);
+    } if (score == compScore){
+        return 'It\'s a tie. Play again!';
+    } else if (score < compScore){
+        return 'Oh, no... You lose!';
+    } else {
+        return 'Congratulations! You win!';
     }
 }
 
- console.log(game());
+console.log(game());
